44f1950cfbab9d8fd9e53a17e8fa4b44203fff5a
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-02-12 21:49:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(firstName)
+
+var firstName = 'Pranav'
+let lastName = 'Mhaske'
+let age = 15
+// let खुशहै = true
+firstName = 100
+var a
+
+const hoursInDay = 24
+
+
+let userIntro = 
+    'Hi, my name is ' 
+    + firstName + ' ' 
+    + lastName + '. I am ' + age + ' ' + 'years old.'
