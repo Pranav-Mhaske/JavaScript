9e40e99fad941d8ff6a5e3902a035671a0fa9247
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-02-23 21:32:50 +0530

    Ternary Operator

---
+++ Day 12/script.js
@@ -0,0 +1,12 @@
+const username = 5 > 2 ? 'Pranav' : 'Manav'
+
+// const gender = 'F'
+
+// debugger
+// const userMessage = `${gender.toLocaleLowerCase() === 'f' ? 'She' : 'He'} is a college student.`
+
+// console.log(userMessage);
+
+const result = null ? 'Manav' : '' ? '12' : 0
+
+console.log(result)
