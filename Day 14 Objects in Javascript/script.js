4a9f663704c095b86a3e3103954cacef92e8e201
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-02-25 20:06:14 +0530

    Objects in JavaScript

---
+++ Day 14 Objects in Javascript/script.js
@@ -0,0 +1,37 @@
+const myName = 'Pranav'
+
+const username1 = ''
+const username2 = ''
+
+const user1 = {
+    firstName: 'Manav'
+}
+
+
+// const user2 = {
+//     firstName: 'Manav',
+//     'last-Name': 'Jadhav',
+//     Pranav: 'Developer'
+// }
+
+// console.log(user2.firstName);
+// console.log(user2["last-Name"]);
+// console.log(user2[myName]);
+// console.log(user2["first" + "Name"]);
+
+const user2 = {
+    firstName: 'Pranav',
+    lastName: 'Mhaske',
+    pata: {
+        city: 'Bangalore',
+        pinCode: 566008,
+        state: 'Karnataka',
+        moreDetails: {
+            population: 8992176898,
+            area: '674 sq km',
+        }
+    }
+}
+
+// user2.age = 26
+// user2["is-student"] = true
