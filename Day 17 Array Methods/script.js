f739fb9ed28a7ef8e3e67eed56319903388d3a26
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-03-01 18:24:37 +0530

    Multidemtional Arrays

--- Day 17 Array Methods/script.js
+++ Day 17 Array Methods/script.js
@@ -20,3 +20,18 @@ console.log(addedArray1) // [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 'Dog
 console.log(addedArray2) // ['Dog', 'Cat', 'Rat', 'Lion', 'Elephant', 'Cow', 'Tiger', 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 1, 3, 5]
 console.log(elementIndex) // 1
 console.log(isIncluded) // false    
+
+animals.shift() // The shift() method removes the first element from an array and returns that removed element. In this case, it will remove 'Dog' from the animals array, and the array will be updated to ['Cat', 'Rat', 'Lion', 'Elephant', 'Cow', 'Tiger'].
+animals.unshift('Dog') // The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array. In this case, it will add 'Dog' back to the beginning of the animals array, and the array will be updated to ['Dog', 'Cat', 'Rat', 'Lion', 'Elephant', 'Cow', 'Tiger'].
+
+
+animals.push('Monkey') // The push() method adds one or more elements to the end of an array and returns the new length of the array. In this case, it will add 'Monkey' to the end of the animals array, and the array will be updated to ['Dog', 'Cat', 'Rat', 'Lion', 'Elephant', 'Cow', 'Tiger', 'Monkey'].
+animals.pop() // The pop() method removes the last element from an array and returns that element. In this case, it will remove 'Monkey' from the end of the animals array, and the array will be updated to ['Dog', 'Cat', 'Rat', 'Lion', 'Elephant', 'Cow', 'Tiger']. 
+
+animals.reverse() // The reverse() method reverses an array in place. The first array element becomes the last, and the last array element becomes the first. In this case, it will reverse the order of the animals array, and the array will be updated to ['Tiger', 'Cow', 'Elephant', 'Lion', 'Rat', 'Cat', 'Dog'].
+animals.sort() // The sort() method sorts the elements of an array in place and returns the sorted array. The default sort order is built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values. In this case, it will sort the animals array in alphabetical order, and the array will be updated to ['Cat', 'Cow', 'Dog', 'Elephant', 'Lion', 'Rat', 'Tiger']. 
+
+animals.splice(2, 1) // The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. In this case, it will remove 1 element starting from index 2 (which is 'Dog') from the animals array, and the array will be updated to ['Cat', 'Cow', 'Elephant', 'Lion', 'Rat', 'Tiger'].     
+animals.splice(2, 2, 'Dog','Giraf',45) // The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. In this case, it will add 'Dog' at index 2 without removing any elements from the animals array, and the array will be updated to ['Cat', 'Cow', 'Dog', 'Giraf', 45, 'Elephant', 'Lion', 'Rat', 'Tiger'].
+
+console.log(animals) // ['Cat', 'Cow', 'Dog', 'Giraf', 45, 'Elephant', 'Lion', 'Rat', 'Tiger']
---
+++ Day 18 Multidemtional Arrays/script.js
@@ -0,0 +1,11 @@
+const nameAndNumberList = [
+  ['Manav', 75],
+  ['Pranav', 90],
+  ['Vaibhav', 9],
+]
+
+const ticTacToe = [
+    ['X', null, null],
+    [null, null, 'O'],
+    ['O', null, 'X']
+]
