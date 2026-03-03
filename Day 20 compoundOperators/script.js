5580f792be53282e97d57c8c53f1cdf05297c65e
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-03-03 22:20:56 +0530

    Compound Operators

---
+++ Day 20 compoundOperators/script.js
@@ -0,0 +1,52 @@
+const username = 'Anurag'
+
+let num = 5
+
+
+num = num + 5
+num = num + 5
+num = num + 5
+num = num + 5
+
+console.log(num) // 25  
+
+let num1 = 5
+num1 += 5
+num1 += 5
+num1 += 5
+num1 += 5
+console.log(num1) // 25 
+
+let num3 = 5
+num3 = num3 * 2
+num3 = num3 * 2
+num3 = num3 * 2
+console.log(num3) // 40
+
+let num4 = 5
+num4 *= 2
+num4 *= 2
+num4 *= 2
+console.log(num4) // 40 
+
+
+let num5 = 100
+num5 = num5 / 2
+num5 = num5 / 2
+num5 = num5 / 2
+console.log(num5) // 12.5
+
+let num6 = 100
+num6 /= 2
+num6 /= 2
+num6 /= 2
+console.log(num6) // 12.5
+
+const newNum1 = ++num
+const newNum2 = --num
+
+
+// const newNum = num++
+// const newNum = num--
+
+num++
