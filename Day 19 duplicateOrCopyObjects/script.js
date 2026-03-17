482f348f753932cae7b5fb492e7f3948b2b57abb
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-03-17 21:26:36 +0530

    Shallow copy of objects and arrays in JavaScript

--- Day 19 duplicateOrCopyObjects/script.js
+++ Day 19 duplicateOrCopyObjects/script.js
@@ -2,7 +2,7 @@
 // let username2 = username1
 // username2 = username2 + ' Singh'
 
-const fruits = ['Mango', 'Apple', 'Orange']
+
 
 // const myFruits = fruits
 
@@ -10,9 +10,18 @@ const fruits = ['Mango', 'Apple', 'Orange']
 // myFruits.push('Grapes')
 
 // Object.assign(myFruits, fruits)
-const myFruits = [...fruits]
+
+const fruits = ['Mango', 'Apple', 'Orange']
+
+const myFruits = [...fruits] // spread operator
+
+myFruits.push('Dates')
+myFruits.push('Grapes')
+
 console.log(fruits) // ['Mango', 'Apple', 'Orange']
-console.log(myFruits) // ['Mango', 'Apple', 'Orange']
+console.log(myFruits) // ['Mango', 'Apple', 'Orange', 'Dates', 'Grapes']
+
+//object assignment and spread operator works only for shallow copy, if there are nested objects or arrays, they will be shared between the original and the copy.
 
 // const myFruits = [].concat(fruits)
 // const myFruits = fruits.slice()
@@ -37,8 +46,10 @@ const user2 = { ...user1 }
 console.log(user2) // { firstName: 'Pranav', lastName: 'Mhaske', pata: { city: 'Pune', pincode: 411038 }, subject: ['Physics', 'CS', 'Maths'] }
 
 user2.firstName = 'Pratik'
-user2.pata.city = 'Pune'
+user2.pata.city = 'Bangalore'
 user2.subject.push('English')
 
-console.log(user1) // { firstName: 'Pranav', lastName: 'Mhaske', pata: { city: 'Pune', pincode: 411038 }, subject: ['Physics', 'CS', 'Maths', 'English'] }
-console.log(user2) // { firstName: 'Pratik', lastName: 'Mhaske', pata: { city: 'Pune', pincode: 411038 }, subject: ['Physics', 'CS', 'Maths', 'English'] }
+// objects insude objects and arrays are reference types, so they will be shared between user1 and user2
+console.log(user1) // { firstName: 'Pranav', lastName: 'Mhaske', pata: { city: 'Bangalore', pincode: 411038 }, subject: ['Physics', 'CS', 'Maths', 'English'] }
+console.log(user2) // { firstName: 'Pratik', lastName: 'Mhaske', pata: { city: 'Bangalore', pincode: 411038 }, subject: ['Physics', 'CS', 'Maths', 'English'] }
+// Shallow copy works only for 1 layer of the object, if there are nested objects or arrays, they will be shared between the original and the copy.
