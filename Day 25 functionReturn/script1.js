b4b995351484560d42e01da697697f6b1a2d1571
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-03-25 23:56:46 +0530

    Returing values from functions is a fundamental concept in programming.

---
+++ Day 25 functionReturn/script1.js
@@ -0,0 +1,21 @@
+// function addTwoNumbers(a, b) {
+// debugger
+
+//     return a + b
+// }
+
+// const result = addTwoNumbers(56, 6)
+
+// console.log(result);
+
+function addTwoNumbers(a, b) {
+    return a + b
+}
+
+const result = addTwoNumbers(56, 6)
+
+console.log(result);
+
+function introduceMe(name, profession, age) {
+    console.log(`Hi, My name is ${name}. I am a ${profession} and I am ${age} years old.`);
+}
