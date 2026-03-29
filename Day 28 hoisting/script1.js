9b608cf11804540584c9c897521a8b7015950c0c
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-03-29 22:54:51 +0530

    Hoisiting in JavaScript

---
+++ Day 28 hoisting/script1.js
@@ -0,0 +1,19 @@
+console.log(a); //undefined
+
+var a = [1, 2, 3, 4, 5] // Array defined
+
+print()
+
+// Function Definition
+// Function Declaration
+function print() {
+    console.log(a);
+}
+
+var printArray = function() { //anonymous function
+    console.log(a);
+}
+
+// IIFE
+
+printArray()
