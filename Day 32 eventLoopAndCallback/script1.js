e43b17e492d2d87d3c673cc7b9e14f7e76ca30bc
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-04-02 23:55:44 +0530

    Callback Queue

---
+++ Day 32 eventLoopAndCallback/script1.js
@@ -0,0 +1,13 @@
+// Callback Queue
+
+console.log('Starting Up!');
+
+setTimeout(() => {
+    console.log('Two Seconds!');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Zero Seconds!');
+}, 0);
+
+console.log('Finishing Up!');
