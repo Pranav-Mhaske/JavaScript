bff637bd01527d6e4d0b8e83a9d33b90b0d3b5de
Pranav-Mhaske <pranavmhaske03@gmail.com>
2026-05-05 22:16:31 +0530

    BAsic Arithimatic Operations

---
+++ Assignments/task.js
@@ -0,0 +1,12 @@
+const a = number(prompt("Enter the first number:"));
+const b = number(prompt("Enter the second number:"));
+
+const sum = a + b;
+const difference = a - b;
+const product = a * b;
+const quotient = a / b;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
